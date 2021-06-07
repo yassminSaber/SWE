5a1c228864422f02650e759644d3e576cf9a25ad
yassminsaber20190620 <84774076+yassminsaber20190620@users.noreply.github.com>
2021-06-08 01:50:41 +0200

    class Playgroun

--- PlaYground.java
+++ PlaYground.java
@@ -1,13 +1,94 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.*;
 
-/**
- *
- * @author lenovo
- */
+
 public class PlaYground {
+    private String location;
+    private String name;
+    private double area;
+    private float pricePerHoure;
+    private double cancellationPeriod;
+    private int start;
+    private int end;
+    private ArrayList<PlaYground> grounds;
+
+    public PlaYground() {
+    }
+    public PlaYground(String location, String name, double area, float pricePerHoure, double cancellationPeriod, int start, int end, ArrayList<PlaYground> grounds) {
+        this.location = location;
+        this.name = name;
+        this.area = area;
+        this.pricePerHoure = pricePerHoure;
+        this.cancellationPeriod = cancellationPeriod;
+        this.start = start;
+        this.end = end;
+        this.grounds = grounds;
+        grounds = new ArrayList<>();
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public void setPricePerHoure(float pricePerHoure) {
+        this.pricePerHoure = pricePerHoure;
+    }
+
+    public void setCancellationPeriod(double cancellationPeriod) {
+        this.cancellationPeriod = cancellationPeriod;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public void setGround(ArrayList<PlaYground> grounds) {
+        this.grounds = grounds;
+    }
+    
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public float getPricePerHoure() {
+        return pricePerHoure;
+    }
+
+    public double getCancellationPeriod() {
+        return cancellationPeriod;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public ArrayList<PlaYground> getGround() {
+        return grounds;
+    }
+    
     
 }
